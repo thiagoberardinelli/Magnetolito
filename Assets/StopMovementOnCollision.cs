28f77db6c07d8fc2ee408a15f7841cd1061f054f
Thiago Braz <thiago.berardinelli@al.infnet.edu.br>
2017-10-27 13:07:04 -0200

    Commit: funcionalidades básicas (Force Manager e bouceness)

---
+++ Assets/StopMovementOnCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopMovementOnCollision : MonoBehaviour
+{
+
+	//void OnCollisionEnter2D(Collision2D col)
+	//{
+
+	//	Debug.Log("Colidiu");
+	//	col.gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+	//	//col.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+	//}
+
+	void OnCollisionStay2D(Collision2D col)
+	{
+		//col.gameObject.GetComponent<AddForce>().enabled = false;
+		col.gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+	
+	}
+}
+
