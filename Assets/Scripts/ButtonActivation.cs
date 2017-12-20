10e87ad675173718ddc71ef0e90898a362383296
Thiago Berardinelli <thiago.berardinelli@al.infnet.edu.br>
2017-12-20 15:13:00 -0200

    Menu Home, Level Selection e "Level 01"

---
+++ Assets/Scripts/ButtonActivation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonActivation : MonoBehaviour {
+
+	private Button button;
+	public Sprite buttonOff;
+	public Sprite buttonOn;
+	private bool active = false;
+
+	public void Start()
+	{
+		button = this.GetComponent<Button>();
+		button.GetComponent<Image>().sprite = buttonOn;
+	}
+
+	public void ButtonStatus()
+	{
+		active = !active;
+
+		if (active == false)
+		{
+			button.GetComponent<Image>().sprite = buttonOn;
+		}
+
+		if (active == true)
+		{
+			button.GetComponent<Image>().sprite = buttonOff;
+		}
+	}
+}
