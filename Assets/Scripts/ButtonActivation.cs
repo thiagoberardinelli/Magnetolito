0d822d2565897d8acf619e8de4c95cbd4d805bf9
Thiago Berardinelli <thiago.berardinelli@al.infnet.edu.br>
2017-12-21 18:34:50 -0200

    Adição do botão voltar em todas as cenas (menos no jogo) e inicio do menu de pause in game.

--- Assets/Scripts/ButtonActivation.cs
+++ Assets/Scripts/ButtonActivation.cs
@@ -5,29 +5,46 @@ using UnityEngine.UI;
 
 public class ButtonActivation : MonoBehaviour {
 
-	private Button button;
-	public Sprite buttonOff;
-	public Sprite buttonOn;
+	public enum ButtonList { SoundEffects, Music }; // Emum que contém os botões presentes no jogo.
+	public ButtonList AudioType; // Nome da button list que contem o enum.
+	private Button button; // O botão a ser referênciado.
+	public Sprite buttonOff; // Sprite do botão Off.
+	public Sprite buttonOn; // Sprite do botão On.
 	private bool active = false;
 
 	public void Start()
 	{
-		button = this.GetComponent<Button>();
-		button.GetComponent<Image>().sprite = buttonOn;
+		button = this.GetComponent<Button>(); // Digo que o botão em questão é o componente botão do objeto que o script está atrelado.
+		button.GetComponent<Image>().sprite = buttonOn; // Por default, os dois botões de áudio começam ativados.
 	}
 
-	public void ButtonStatus()
+	public void ButtonMusicStatus()
 	{
-		active = !active;
+
+		// Condição que troca os sprites dos botões. Essa ação é comum aos dois botões, dessa forma é chamada sempre pela função. 
+		active = !active; 
 
 		if (active == false)
 		{
 			button.GetComponent<Image>().sprite = buttonOn;
 		}
-
+			
 		if (active == true)
 		{
 			button.GetComponent<Image>().sprite = buttonOff;
 		}
+
+		// Switch responsável por determinar qual botão está sendo chamado pelo enum. Perceba que cada botão, tem também sua função individual. 
+		switch (AudioType) 
+		{
+			case ButtonList.SoundEffects:
+				Debug.Log("SoundEffects");
+				break;
+			case ButtonList.Music:
+				Debug.Log("Music");
+				break;
+		}
 	}
+	
 }
+
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
+	// Função que faz a troca de cenas. A referência é feita pelo nome da Cena.
 	public void GoToLevel(string levelName)
 	{
 		SceneManager.LoadScene(levelName);
 	}
 
+	// Função que fecha o aplicativo
 	public void ExitGame()
 	{
 		Application.Quit();
