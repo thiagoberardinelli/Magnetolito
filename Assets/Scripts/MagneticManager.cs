01cdf2d049a7c19efd9758a9e20c76944627749d
Thiago Baptista <contato@thiagobaptista.com>
2017-12-26 17:10:39 -0200

    Core systems revamp.

--- Assets/Scripts/MagneticManager.cs
+++ Assets/Scripts/MagneticManager.cs
@@ -7,35 +7,21 @@ public class MagneticManager : MonoBehaviour
 	public List<GameObject> AffectedObjects = new List<GameObject>();
 
 	public float MagneticForceIntensity = 1f;
-	Vector3? PrevDirection;
+
 	void Update()
 	{
 		if (Input.GetMouseButton(0))
 		{
-			Debug.Log("Mouse position: " + Input.mousePosition);
 			Vector3 Direction = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Camera.main.ScreentoWorld point retona o valor da posição do mouse em relação ao mundo.
-			Debug.Log("Direction: " + Direction);
 
 			foreach (GameObject Item in AffectedObjects)
 			{
-				Vector3 DirectionResultant = Direction - Item.transform.position; // Resultante da posição de onde o mouse na hora do clique menos a posição atual dele (em outras palavras, para onde o círculo 
-				if (PrevDirection.HasValue) // Checa se o movimento anterior é diferente de nulo.
-				{
-					Item.GetComponent<Rigidbody2D>().velocity = Vector2.zero; // Freia o movimento antes de começar um novo.
-				}
+				Vector3 DirectionResultant = Direction - Item.transform.position; // Resultante da posição de onde o mouse na hora do clique menos a posição atual dele (em outras palavras, para onde o círculo
+                DirectionResultant.Normalize(); // So that, no matter where on the screen the player touches, the resulting force will be the same
 
 				Item.GetComponent<Rigidbody2D>().AddForce(DirectionResultant * MagneticForceIntensity); // Aplicação da força nos items da lista. A variável MagneticForceIntensy é a constante que influência na "velocidade" de movimentos dos itens.
-				PrevDirection = DirectionResultant;
 			}
 		}
-
-		// Conversar com o Thiagão o objeto ter que seguir o outro depois que o jogador parar de aplicar a força. Um deve simplesmente
-
-		//else
-		//{
-		//	AffectedObjects[0].transform.position = AffectedObjects[1].transform.position;
-
-		//}
 	}
 }
 
