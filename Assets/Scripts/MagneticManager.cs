fe996d3dd1725553f12e6a019063619349e25da4
Thiago Baptista <contato@thiagobaptista.com>
2018-02-19 22:50:55 -0300

    Refactored the magnetic force logic; now, each moveable object is responsible for applying the force on itself

--- Assets/Scripts/MagneticManager.cs
+++ Assets/Scripts/MagneticManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MagneticManager : MonoBehaviour
 {
-	public List<GameObject> AffectedObjects = new List<GameObject>();
+	public List<MovableObject> AffectedObjects = new List<MovableObject>();
 
 	public float MagneticForceIntensity = 1f;
 
@@ -14,12 +14,9 @@ public class MagneticManager : MonoBehaviour
 		{
 			Vector3 Direction = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Camera.main.ScreentoWorld point retona o valor da posição do mouse em relação ao mundo.
 
-			foreach (GameObject Item in AffectedObjects)
+			foreach (MovableObject Item in AffectedObjects)
 			{
-				Vector3 DirectionResultant = Direction - Item.transform.position; // Resultante da posição de onde o mouse na hora do clique menos a posição atual dele (em outras palavras, para onde o círculo
-                DirectionResultant.Normalize(); // So that, no matter where on the screen the player touches, the resulting force will be the same
-
-				Item.GetComponent<Rigidbody2D>().AddForce(DirectionResultant * MagneticForceIntensity); // Aplicação da força nos items da lista. A variável MagneticForceIntensy é a constante que influência na "velocidade" de movimentos dos itens.
+                Item.ApplyForce(Direction, MagneticForceIntensity);
 			}
 		}
 	}
---
+++ Assets/Scripts/MovableObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableObject : MonoBehaviour
+{
+	public void ApplyForce(Vector3 Direction, float ForceIntensity)
+    {
+        Vector3 DirectionResultant = Direction - transform.position;
+        DirectionResultant.Normalize(); // So that, no matter where on the screen the player touches, the resulting force will be the same
+
+        GetComponent<Rigidbody2D>().AddForce(DirectionResultant * ForceIntensity);
+    }
+}
