23f39366f25216ddecc41f50168867d582fd2631
Thiago Berardinelli <thiago.berardinellI@al.infnet.edu.br>
2018-05-23 01:16:10 -0300

    - New backgrounds added to assets folder - The UI Menu Button now has a transparency effect if not clicked after 3 seconds

--- Assets/Scripts/MenuBehavior.cs
+++ Assets/Scripts/MenuBehavior.cs
@@ -5,18 +5,43 @@ using UnityEngine.UI;
 
 public class MenuBehavior : MonoBehaviour
 {
+    private bool isMenuButtonsActive;
+    private Image buttonImage;
+    private Color buttonColor;
+    private float colorVariation = 0.25f;
 
-	private GameObject menuButton;
-	public List<GameObject> menuContent = new List<GameObject>();
-	private bool isMenuButtonsActive;
+    private float timer = 0f;
+    private float timerLimit = 3f;
+    private float timerVariation = 1f;
+    	
+    public List<GameObject> menuContent = new List<GameObject>();
+	
 
-	void Start()
+	private void Start()
 	{
-		menuButton = GetComponent<GameObject>();
-	}
+        buttonImage = GetComponent<Image>();
+        buttonColor = buttonImage.color;
+   	}
 
-	// Function that checks the currently state of the button slider. Must be public because it's called in the inspector. 
-	public void MenuSlider()
+    private void Update()
+    {
+        if (timer < timerLimit)
+        {
+            timer += timerVariation * Time.deltaTime;
+        }
+
+        if (timer >= timerLimit)
+        {
+            if (buttonColor.a >= 0.5f && isMenuButtonsActive == false)
+            {
+                buttonColor.a -= colorVariation * Time.deltaTime;
+                buttonImage.color = buttonColor;
+            }
+        }
+    }
+
+    // Function that checks the currently state of the button slider. Must be public because it's called in the inspector. 
+    public void MenuSlider()
 	{
 		if (isMenuButtonsActive == false)
 		{
@@ -37,6 +62,10 @@ public class MenuBehavior : MonoBehaviour
 			Item.SetActive(true);
 		}
 		isMenuButtonsActive = true;
+
+        // Sets back the alpha of the button to its maximum.
+        buttonColor.a = 1f;
+        buttonImage.color = buttonColor;
 	}
 
 	// Function that deactivates the menu slider if its on.
@@ -47,5 +76,8 @@ public class MenuBehavior : MonoBehaviour
 			Item.SetActive(false);
 		}
 		isMenuButtonsActive = false;
+
+        // Restarts the timer so it can counts 3 seconds before the button fades again.
+        timer = 0f;
 	}
 }
