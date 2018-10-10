1569d0af6973ddb407b139ff749b99b986b13933
DESKTOP-H991CVQ\Thiago Berardinelli <thiago.berardinelli@al.infnet.edu.br>
2018-10-10 19:21:58 -0300

    First two scenes deleted (Slapsh Screen and Intro) and new Level Selection Menu mechanism implemented.

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public List<Level> levels;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        
+        // Listar aqui todos os levels.
+        levels = new List<Level>
+        {
+            new Level(0, "Level00", true, 3, false),
+            new Level(1, "Level01", false, 0, true),
+            new Level(2, "Level02", false, 0, true),
+            new Level(3, "Level03", false, 0, true),
+            new Level(4, "Level04", false, 0, true),
+            new Level(5, "Level05", false, 0, true),
+            new Level(6, "Level06", false, 0, true),
+            new Level(7, "Level07", false, 0, true),
+            new Level(8, "Level08", false, 0, true),
+            new Level(9, "Level09", false, 0, true),
+            new Level(10, "Leve10", false, 0, true),
+            new Level(11, "Leve11", false, 0, true),
+            new Level(12, "Leve12", false, 0, true),
+            new Level(12, "Leve13", false, 0, true)
+        };
+    }
+
+    public void StartLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void CompleteLevel(string levelName)
+    {
+        levels.Find(i => i.LevelName == levelName).Complete(); // Procura na lista de Levels o level correspondente e muda seu status para completo.
+    }
+
+    public void CompleteLevel(string levelName, int stars)
+    {
+        levels.Find(i => i.LevelName == levelName).Complete(stars); // Igual o de cima mas tambem colocar o numero de estrelas obtidas no final do level.
+    }
+
+    public void LockLevel(string levelName)
+    {
+        levels.Find(i => i.LevelName == levelName).Lock();
+    }
+
+    public void UnlockLevel(string levelName)
+    {
+        levels.Find(i => i.LevelName == levelName).Unlock();
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UILevelSelect.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class UILevelSelect : MonoBehaviour
+{
+    [SerializeField] private LevelController levelController;
+    [SerializeField] private UILevel levelUI;
+    //[SerializeField] private GameObject levelPopUp;
+
+    private Transform levelSelectPanel;
+    private int currentPage;
+    private float pageNumber;
+    private List<UILevel> levelList = new List<UILevel>(); // Lista de todos os botoes que representam os levels
+
+    void Start()
+    {
+        levelSelectPanel = transform;
+
+        for (int i = 0; i < levelController.levels.Count; i++) // Para cada Level que eu tenho eu vou adicionar um levelUI (UILevel script) na levelList.
+        {
+            levelList.Add(levelUI);
+        }
+        BuildLevelPage(0); // Primeira pagina deve aparecer quando o jogo acontece.
+    }
+
+    void BuildLevelPage(int page) // Metodo que instancia os elementos de acordo com a pagina correspondente
+    {
+        RemoveItensFromPage();
+        currentPage = page;
+        int pageSize = 10; // Quantidade de levels por pagina.
+        pageNumber = Mathf.Round(levelList.Count / pageSize);
+
+        // Pula o numero correspondente de elementos de acordo com a pagina e conta os 10 proximos (pageSize)
+        List<UILevel> pageList = levelList.Skip(page * pageSize).Take(pageSize).ToList();
+
+        for (int i = 0; i < pageList.Count; i++)
+        {
+            Level level = levelController.levels[(page * pageSize) + i]; // para cada i eu estou criando um Level correspondente.
+            UILevel instance = Instantiate(pageList[i]);
+
+            
+            instance.SetStars(level.Stars);
+            instance.transform.SetParent(levelSelectPanel);
+            instance.GetComponent<RectTransform>().localScale = Vector3.one;
+            instance.GetComponent<Button>().onClick.AddListener(() => SelectLevel(level));
+
+            if (!level.Locked) // Se nao esta bloqueado.
+            {
+                instance.lockImage.SetActive(false);
+                instance.levelTextID.text = level.ID.ToString();
+            }
+            else
+            {
+                instance.lockImage.SetActive(true);
+                instance.levelTextID.text = "";
+            }
+        }
+    }
+
+    void RemoveItensFromPage() // Metodo que destroi a pagina anterior.
+    {
+        for (int i = 0; i < levelSelectPanel.childCount; i++)
+        {
+            Destroy(levelSelectPanel.GetChild(i).gameObject); 
+        }
+    }
+
+    public void NextPage()
+    {
+        if (currentPage < pageNumber)
+        {
+            BuildLevelPage(currentPage + 1);
+        }
+    }
+
+    public void PreviousPage()
+    {
+        if (currentPage > 0)
+        {
+            BuildLevelPage(currentPage - 1);
+        }
+    }
+
+    void SelectLevel(Level level) // Metodo que checa se o level ta trancado ou nao no clique. Se sim, ele abre o popup e se nao, ele loada o level clicado.
+    {
+        if (level.Locked)
+        {
+            Debug.Log("Level is locked!");
+        }
+
+        else
+        {
+            levelController.StartLevel(level.LevelName);
+        }
+    }
+}
